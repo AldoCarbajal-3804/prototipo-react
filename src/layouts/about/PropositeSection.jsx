e6645347d3dcafe737edcfa4aaeeaeb7eaa41914
AldoCarbajal-3804 <aldocarbajal62@gmail.com>
2026-05-07 12:16:52 -0500

    Responsividad relativa

--- src/layouts/about/PropositeSection.jsx
+++ src/layouts/about/PropositeSection.jsx
@@ -9,7 +9,7 @@ function PropositeSection() {
     return(
         <section className="relative mx-auto py-0 border-b-2 border-gray-500 bg-blue-100">
 
-            <main className="relative max-w-7xl mx-auto py-16 sm:py-20 lg:py-24 grid grid-cols-1 md:grid-cols-2 gap-8 sm:gap-10 px-4 sm:px-6">
+            <main className="relative max-w-7xl mx-auto py-[4%] grid grid-cols-1 md:grid-cols-2 gap-8 sm:gap-10 px-[2%] sm:px-6">
                 
                 <div
                     ref={missionRef}
